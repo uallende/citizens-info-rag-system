163db478508b216e712390781475969d368bcf5f
root <allende.rev@gmail.com>
2024-04-22 16:55:07 +0100

    llm chached

--- app/rag.py
+++ app/rag.py
@@ -3,13 +3,22 @@ import weaviate.classes as wvc
 import os
 import torch.nn.functional as F
 import torch, gc
-from dotenv import load_dotenv
+import streamlit as st
 
 from weaviate.classes.query import MetadataQuery
-from transformers import BitsAndBytesConfig, AutoModel, AutoTokenizer, BitsAndBytesConfig
+from transformers import BitsAndBytesConfig, AutoModel, AutoTokenizer, BitsAndBytesConfig, AutoConfig, AutoModelForCausalLM
 from torch import Tensor
+from dotenv import load_dotenv
+from app.constants import *
+from sentence_transformers import SentenceTransformer
 
 
+def weaviate_client():
+    client = weaviate.connect_to_local(
+        port=8080,
+        grpc_port=50051,
+        additional_config=weaviate.config.AdditionalConfig(timeout=(60, 180)))
+    return client
 
 def last_token_pool(last_hidden_states: Tensor,
                  attention_mask: Tensor) -> Tensor:
@@ -21,15 +30,36 @@ def last_token_pool(last_hidden_states: Tensor,
         batch_size = last_hidden_states.shape[0]
     return last_hidden_states[torch.arange(batch_size, device=last_hidden_states.device), sequence_lengths]
 
-def generate_text_embeddings(text:str, model, tokenizer, max_length):
+def generate_text_embeddings(text:str):
 
-    batch_dict = tokenizer(text, max_length=max_length, padding=True, truncation=True, return_tensors="pt").to('cuda')
+    max_length = 4096
+    model = load_embeddings_model()
+    tokenizer = AutoTokenizer.from_pretrained('Salesforce/SFR-Embedding-Mistral')
+    batch_dict = tokenizer(text, max_length=max_length, padding=True, truncation=True, return_tensors="pt").to(DEVICE)
     model.eval()
     output = model(**batch_dict)
 
     with torch.no_grad():
         embeddings = last_token_pool(output.last_hidden_state, batch_dict['attention_mask'])[0].float().cpu().detach().numpy()
+    clear_gpu_memory(model, tokenizer)
     return embeddings
+
+def retrieve_nearest_content(collection, query_embeddings):
+        
+    response = collection.query.near_vector(
+        near_vector=query_embeddings.tolist(),  
+        target_vector='default', 
+        return_properties=['body', 'title'],
+        limit=2,
+        return_metadata=MetadataQuery(distance=True)
+    )
+    return response
+
+def clear_gpu_memory(model, tokenizer):
+    model.cpu()
+    del model, tokenizer
+    torch.cuda.empty_cache()
+    gc.collect()
 
 def load_embeddings_model():
 
@@ -43,9 +73,79 @@ def load_embeddings_model():
     model = AutoModel.from_pretrained(
         'Salesforce/SFR-Embedding-Mistral',
         trust_remote_code=True,
-        device_map='cuda',
+        device_map=DEVICE,
         torch_dtype=torch.bfloat16,
         quantization_config=bnb_config
     )
 
     return model
+
+@st.cache(allow_output_mutation=True)
+def load_llm():
+
+    model_name_or_path = "mistralai/Mistral-7B-Instruct-v0.2"
+
+    config = AutoConfig.from_pretrained(model_name_or_path, trust_remote_code=True)
+    config.max_position_embeddings = 8096
+    quantization_config = BitsAndBytesConfig(
+    llm_int8_enable_fp32_cpu_offload=True,
+    bnb_4bit_quant_type='nf4',
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    load_in_4bit=True
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
+    model = AutoModelForCausalLM.from_pretrained(
+    model_name_or_path,
+    config=config,
+    trust_remote_code=True,
+    quantization_config=quantization_config,
+    device_map="cuda",
+    offload_folder="./offload"
+    )
+
+    return model, tokenizer
+
+def generate_final_answer(context, user_input):
+
+    model, tokenizer = load_llm()
+    prompt = [
+        {
+
+        "role": "user", 
+        "content": (
+            f"Based on the following context {context}, "
+            f"can you provide an answer to this {user_input}. "
+            f"If the information is not clear say I don't know but don't make up any information"
+                    ),
+
+         }
+            ]   
+    
+    encoded_prompt = tokenizer.apply_chat_template(prompt, return_tensors="pt").to(DEVICE)
+    generated_ids = model.generate(encoded_prompt, max_new_tokens=1000, do_sample=True, pad_token_id=tokenizer.eos_token_id)  # Use encoded_prompt directly
+    decoded = tokenizer.batch_decode(generated_ids)
+    return decoded
+
+def parse_llm_generated_answer(decoded):
+    # Extract the answer after the [/INST] token
+    start_token = "[/INST]"
+    start_index = decoded[0].find(start_token)
+
+    if start_index != -1:
+        start_index += len(start_token)
+        answer = decoded[0][start_index:].strip().replace("</s>", "")
+
+    else:
+        return None
+    return answer
+
+def lightweight_embedding_model():
+    return SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+@st.cache(allow_output_mutation=True)
+def generate_lightweight_embeddings(text:str):
+    model = lightweight_embedding_model()
+    embeddings = model.encode(text)
+    return embeddings
